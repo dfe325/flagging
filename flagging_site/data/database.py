c8ee202ef0a9cd5f68014ace00c3dd15a505edc8
dfe325 <daneder@gmail.com>
2020-06-23 12:53:23 -0400

    update database.py, add pw to DB URL

--- flagging_site/data/database.py
+++ flagging_site/data/database.py
@@ -3,3 +3,21 @@ This file should handle all database connection stuff, namely: writing and
 retrieving data.
 """
 
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(database=DATABASE_URL, user='postgres', host='localhost', password='postgrespw', port=5432)
+
+#conn.close()
+
+print(conn.closed)
+
+try:
+    cur = conn.cursor()
+    cur.execute('SELECT 1')
+except psycopg2.OperationalError:
+    pass
+
+print(conn.closed) # 2
