89effe0be9bbc5932c9f8b4941730ebd9a45cfb7
Dan Eder <daneder@gmail.com>
2020-07-28 17:14:08 -0400

    3rd iteration, Issue56

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,6 +7,7 @@ outputs are what you expect them to be.
 from flagging_site.blueprints.api import model_api
 from flagging_site.data.model import reach_2_model
 from flagging_site.data.model import process_data
+from flagging_site.blueprints.flagging import output_model
 from flagging_site.data.usgs import get_live_usgs_data
 from flagging_site.data.hobolink import get_live_hobolink_data
 
@@ -22,12 +23,12 @@ def test_model_output_api_schema(app):
             assert type(key) is list(schema.keys())[0]
             assert isinstance(value, list)
 
-def test_model_output_api_parameters(app):
-    pass
+def test_model_output_api_parameters():
     """test_model_output_api_parameters() should test that hours=10 returns 10 rows of data,
     that setting the reach returns only that reach."""
     df = reach_2_model(process_data(df_usgs=get_live_usgs_data(), df_hobolink=get_live_hobolink_data('code_for_boston_export_21d')), 10)
     row_count = len(df)
-    assert row_count is 10
-    #assert <setting the reach returns only that reach>
-
+    assert row_count == 10
+    test_reach = model_api([3], '10')
+    test_reach = list(test_reach.values())
+    assert 'reach_3' == test_reach[2]
